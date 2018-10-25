5291d3baf9d0de7f08b3af854cec4ffdf7e4c7e5
tim.schierwater <tim.schierwater@student.fh-kiel.de>
2018-10-25 20:17:35 +0200

    sequelize successfully implemented

--- APE/backend/index.js
+++ APE/backend/index.js
@@ -3,10 +3,80 @@ const mysql = require('mysql');
 
 const app = express();
 
+//Including dependency
+const Sequelize = require('sequelize');
+
+//Setting up the config
+const sequelize = new Sequelize('ape', 'root', 'root', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+//Checking connection status
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+//Create Item Table Structure
+var Item = sequelize.define('Item', {
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    name:Sequelize.STRING,
+    description: Sequelize.STRING,
+    qty: Sequelize.INTEGER
+});
+
+sequelize.sync();
+Item.findAll().then(function (user) {        //{ where: { id: '1' } }
+    console.log("Reading data - succesfully");          //transactions have to be included because of multiple access
+    console.log(user)                                      //.dataValues.get({plain:true})
+}).error(function (err) {
+    console.log("Error:" + err);
+});
+/*
+Item.findAll().then(function(insertedItem){
+    console.log(insertedItem.dataValues)
+});
+*/
+//##############################Insert Data - refresh table in data window!#######################################################
+//There is two way of inserting data into database
+//One way: Forming object from modal
+/*
+var item1 = Item.build({
+    id: 2,
+    name:'PC',
+    description: 'MAC 2340TL',
+    qty: 23
+});
+//Inserting Data into database
+item1.save();
+
+//Second way: create() equals build() + save()
+Item.create({
+    id: 2,
+    name: 'PC',
+    description: 'MAC 2340TL',
+    qty: 23
+}).then(function(insertedItem){
+    console.log(insertedItem.dataValues)
+});
+*/
+//#####################################################################################
+
+//---------------------------without Sequelize = old version------------------------------------------
+/*
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'leberkaese',
+    password: 'root',
     database: 'ape'
 })
 
@@ -37,7 +107,7 @@ app.get("/api/members", (req, res) => {
     })
 
 });
-
+*/
 
 const port = 5000;
 
