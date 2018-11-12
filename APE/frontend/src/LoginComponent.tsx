83ec2094be0465546e377d3a7161043beb4e8a81
glen.j.wider <glen.j.wider@student.fh-kiel.de>
2018-11-12 11:10:13 +0100

    layout changed to centered

--- APE/frontend/src/LoginComponent.tsx
+++ APE/frontend/src/LoginComponent.tsx
@@ -1,65 +1,69 @@
-import * as React from 'react';
+import * as React from "react";
 import InputLabel from "@material-ui/core/es/InputLabel/InputLabel";
 import FormControl from "@material-ui/core/es/FormControl/FormControl";
 import FilledInput from "@material-ui/core/es/FilledInput/FilledInput";
-import './LoginComponent.css';
+import "./LoginComponent.css";
 import Button from "@material-ui/core/es/Button/Button";
 
-
-interface Props {
-
-}
+interface Props {}
 
 interface State {
-    email: ''
-    passwort: ''
+  email: "";
+  passwort: "";
 }
 
 class LoginComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      email: "",
+      passwort: ""
+    };
+  }
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            email: '',
-            passwort: ''
-        }
-    }
+  render() {
+    const { email, passwort } = this.state;
 
+    const handleChange = (event: any) => {
+      const target = event.currentTarget;
+      const value = target.value;
+      const name = target.name;
+      this.setState({ [name]: value } as State);
+    };
 
+    const handleSubmit = () => {
+      console.log("submit funktioniert", this.state);
+    };
 
-    render() {
-        const {email , passwort } = this.state
+    return (
+      <div className={"inputForm"}>
+        <h1 className={"header"}>Entwicklungsbogentool</h1>
+        <FormControl className={"emailForm"} variant="filled">
+          <InputLabel htmlFor="component-filled">E-Mail</InputLabel>
+          <FilledInput name="email" value={email} onChange={handleChange} />
+        </FormControl>
 
-        const handleChange = (event: any) => {
-            const target = event.currentTarget
-            const value = target.value
-            const name = target.name
-            this.setState( { [name]: value} as State)
-        };
+        <FormControl className={"passwordForm"} variant="filled">
+          <InputLabel htmlFor="component-filled">Passwort</InputLabel>
+          <FilledInput
+            name="passwort"
+            value={passwort}
+            onChange={handleChange}
+            type={"password"}
+          />
+        </FormControl>
 
-        const handleSubmit = () => {
-            console.log("submit funktioniert", this.state)
-        }
-
-        return (
-            <div className={'inputForm'}>
-                <h1>Entwicklungsbogentool</h1>
-                <FormControl className={'emailForm'} variant="filled">
-                    <InputLabel htmlFor="component-filled">E-Mail</InputLabel>
-                    <FilledInput name='email' value={email} onChange={handleChange}/>
-                </FormControl>
-
-                <FormControl className={'passwordForm'} variant="filled">
-                    <InputLabel htmlFor="component-filled">Passwort</InputLabel>
-                    <FilledInput name='passwort' value={passwort} onChange={handleChange} type={"password"}/>
-                </FormControl>
-
-                <Button variant={"contained"} color={"primary"} className={"submitButton"} onClick={handleSubmit}>
-                    Login
-                </Button>
-            </div>
-        );
-    }
+        <Button
+          variant={"contained"}
+          color={"primary"}
+          className={"submitButton"}
+          onClick={handleSubmit}
+        >
+          Login
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default LoginComponent
+export default LoginComponent;
