2a8e464fe05527a5c20ddf60468a032e8d68cde9
Steven Jonscher <steven.jonscher@gmail.com>
2018-11-12 15:44:56 +0100

    Changed message attribute

--- APE/frontend/src/redux/actions/alert.ts
+++ APE/frontend/src/redux/actions/alert.ts
@@ -1,11 +1,11 @@
 import { alertConstants } from "../../constants";
 
 export const successAlert = (msg: String) => {
-  return { type: alertConstants.SUCCESS, msg };
+  return { type: alertConstants.SUCCESS, message: msg };
 };
 
 export const errorAlert = (msg: String) => {
-  return { type: alertConstants.ERROR, msg };
+  return { type: alertConstants.ERROR, message: msg };
 };
 
 export const clearAlert = () => {
