6aac0a8838be52febfcbbb3626e7e583fd560afa
Steven Jonscher <steven.jonscher@gmail.com>
2018-11-14 11:39:48 +0100

    Auto logout after timeout

--- APE/frontend/src/components/Home/HomeComponent.tsx
+++ APE/frontend/src/components/Home/HomeComponent.tsx
@@ -1,11 +1,45 @@
 import * as React from "react";
 import { Navigation } from "../Navigation/Navigation";
+import IdleTimer from "react-idle-timer";
+import { Redirect } from "react-router";
 
-export const Home = () => {
-  return (
-    <div>
-      <Navigation />
-      Home
-    </div>
-  );
-};
+interface Props {}
+
+interface State {
+  redirect: boolean;
+}
+
+export class Home extends React.Component<Props, State> {
+  public idleTimer: any;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      redirect: false
+    };
+
+    this.idleTimer = null;
+  }
+  render() {
+    const { redirect } = this.state;
+    return (
+      <IdleTimer
+        ref={ref => {
+          this.idleTimer = ref;
+        }}
+        element={document}
+        onIdle={this._onIdle}
+        timeout={15 * 60 * 100}
+      >
+        {redirect && <Redirect to={"/login"} />}
+        <Navigation />
+        Home
+      </IdleTimer>
+    );
+  }
+
+  private _onIdle = () => {
+    this.setState({ redirect: true });
+  };
+}
