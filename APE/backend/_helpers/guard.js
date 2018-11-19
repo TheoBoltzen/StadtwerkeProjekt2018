6944c61716d7a0c04aff5477effc068c1a4a95c2
Steven Jonscher <steven.jonscher@gmail.com>
2018-11-19 15:10:36 +0100

    Move verify function into helpers

---
+++ APE/backend/_helpers/guard.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const config = require("./../config.json"); //have to be implemented
+
+module.exports = role => (req, res, next) => {
+  const token = req.headers.authorization.split("Bearer ")[1];
+  const verifiedToken = jwt.verify(token, config.secret);
+
+  const permission = verifiedToken.permissions;
+
+  const isPermitted = !!role.find(i => i === permission);
+
+  if (isPermitted) {
+    next();
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
+};
--- APE/backend/routes/items.route.js
+++ APE/backend/routes/items.route.js
@@ -1,28 +1,7 @@
+const Items = require("../controllers/items.controller.js");
+const guard = require("../_helpers/guard");
+
 module.exports = app => {
-  const Items = require("../controllers/items.controller.js");
-  const jwt = require("jsonwebtoken");
-  const config = require("./../config.json"); //have to be implemented
-  const error = require("./../_helpers/error-handler");
-
   //Get all Items
-  app.get(
-    "/api/items",
-    (req, res, next) => {
-      const token = req.headers.authorization.split("Bearer ")[1];
-      const decodedToken = jwt.decode(token, config.secret);
-
-      const role = decodedToken.permissions;
-      console.log("req", token);
-      console.log("decodeRoute", decodedToken);
-      console.log("permission: ", role);
-
-      if (role === "admin") {
-        next();
-      } else {
-        res.send(error());
-        console.log("error");
-      }
-    },
-    Items.findAll
-  );
+  app.get("/api/items", guard(["admin"]), Items.findAll);
 };
