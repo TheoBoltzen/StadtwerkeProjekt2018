5d322132e39403e2282cd65b774c1fe493ece96d
glen.j.wider <glen.j.wider@student.fh-kiel.de>
2018-11-26 13:01:53 +0100

    DevelopmentForm creation on page rather than modal, refactored

--- APE/frontend/src/components/DevelopmentForms/DevelopmentForms.tsx
+++ APE/frontend/src/components/DevelopmentForms/DevelopmentForms.tsx
@@ -1,12 +1,42 @@
 import * as React from "react";
-import ModalButtonWrapped from "./ModalButton";
 import "./DevelopmentForms.css";
+import Button from "@material-ui/core/es/Button/Button";
+import DevelopmentStepper from "./DevelopmentStepper";
 
-export const DevelopmentForms = () => {
-  return (
-    <div className={"root"}>
-      <div>DevelopmentForms</div>
-      <ModalButtonWrapped />
-    </div>
-  );
-};
+interface state {
+  visibilityIndex: boolean;
+}
+
+interface Props {}
+
+export class DevelopmentForms extends React.Component<Props, state> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      visibilityIndex: false
+    };
+  }
+
+  changeVisibilityIndex = () => {
+    this.setState({ visibilityIndex: !this.state.visibilityIndex });
+  };
+
+  getContent = () => {
+    return this.state.visibilityIndex ? (
+      <div className={"switchRoot"}>
+        <Button onClick={this.changeVisibilityIndex}>Zurück</Button>
+        <DevelopmentStepper />
+      </div>
+    ) : (
+      <div className={"switchRoot"}>
+        <div>DevelopmentForms</div>
+        <Button onClick={this.changeVisibilityIndex}>Entwicklungsbogen erstellen</Button>
+      </div>
+    );
+  };
+
+  render() {
+    return <div className={"root"}>{this.getContent()}</div>;
+  }
+}
