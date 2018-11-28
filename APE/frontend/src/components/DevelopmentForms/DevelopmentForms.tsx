a55c5585d000e0a967ef1214114ac57652985636
tim.schierwater <tim.schierwater@student.fh-kiel.de>
2018-11-28 11:46:15 +0100

    list of developmentsheets ready

--- APE/frontend/src/components/DevelopmentForms/DevelopmentForms.tsx
+++ APE/frontend/src/components/DevelopmentForms/DevelopmentForms.tsx
@@ -1,5 +1,54 @@
 import * as React from "react";
+//import "../Login/LoginComponent.css";
+import "./DevelopmentForms.css";
+import { ListItem } from "./ListItem";
+import { User } from "../../types/user-types";
+
+/*interface Props {
+    //developmentSheets: developmentSheets[];
+    users: User[];
+}*/
 
 export const DevelopmentForms = () => {
-  return <div>DevelopmentForms</div>;
+  //const { developmentSheets } = props;
+  const user: User[] = [
+    {
+      firstname: "Steven",
+      lastname: "Hallo",
+      role: "admin",
+      token: "jdjdhf",
+      username: "username"
+    },
+    {
+      firstname: "Franz",
+      lastname: "Müller",
+      role: "trainee",
+      token: "jdjdhf",
+      username: "username_franz"
+    }
+  ];
+
+  return (
+    <div className={"frame center"}>
+      <ListItem
+        isHeader={true}
+        name="Name"
+        abteilung="Abteilung"
+        job="Ausbildungsberuf"
+        date="Erstellungsdatum"
+        version="Version"
+      />
+      {user.map(user => {
+        return (
+          <ListItem
+            name={user.firstname}
+            abteilung={user.lastname}
+            job={user.role}
+            date={user.token}
+            version={user.username}
+          />
+        );
+      })}
+    </div>
+  );
 };
