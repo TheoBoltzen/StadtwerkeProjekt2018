66a4f18134c76613edf7ecc2a2630d5a98f5abe5
glen.j.wider <glen.j.wider@student.fh-kiel.de>
2018-12-05 09:26:41 +0100

    Merge branch 'devBranch' into feature/kompetenzkategorien
    # Conflicts:
    #	APE/frontend/src/components/DevelopmentForms/DevelopmentForms.css
    #	APE/frontend/src/components/DevelopmentForms/DevelopmentForms.tsx
    #	APE/frontend/src/components/Home/HomeComponent.tsx

--- APE/frontend/src/components/DevelopmentForms/DevelopmenFormsComponent.tsx
+++ APE/frontend/src/components/DevelopmentForms/DevelopmenFormsComponent.tsx
@@ -1,49 +1,74 @@
 import * as React from "react";
 import "./DevelopmentForms.css";
+import Button from "@material-ui/core/es/Button/Button";
+import DevelopmentStepper from "./DevelopmentStepper";
 import { ListItem } from "./ListItem";
-import { AllProps } from "./DevelopmentForms";
+import { AllProps, State } from "./DevelopmentForms";
 import { CircularProgress } from "@material-ui/core";
 
-export class DevelopmentFormsComponent extends React.Component<AllProps, {}> {
+export class DevelopmentFormsComponent extends React.Component<AllProps, State> {
   constructor(props: AllProps) {
     super(props);
+
+    this.state = {
+      visibilityIndex: false
+    };
   }
 
   componentDidMount() {
     this.props.getAllDevForms();
   }
 
+  changeVisibilityIndex = () => {
+    this.setState({ visibilityIndex: !this.state.visibilityIndex });
+  };
+
   doFormatDate = date => {
     //date format from db: 	2018-12-03T12:12:26.000Z
     return new Date(date).toLocaleDateString("de");
   };
 
-  render() {
+  getContent = () => {
     const { developmentForms, loading } = this.props;
 
-    return loading ? (
-      <CircularProgress />
+    return this.state.visibilityIndex ? (
+      <div className={"switchRoot"}>
+        <Button onClick={this.changeVisibilityIndex}>Zurück</Button>
+        <DevelopmentStepper />
+      </div>
     ) : (
-      <div className={"frame center"}>
-        <ListItem
-          isHeader={true}
-          abteilung="Abteilung"
-          job="Ausbildungsberuf"
-          date="Erstellungsdatum"
-          version="Version"
-        />
-        {developmentForms.map((devForm, index) => {
-          return (
+      <div className={"switchRoot"}>
+        <Button onClick={this.changeVisibilityIndex}>Entwicklungsbogen erstellen</Button>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <div className={"frame center"}>
             <ListItem
-              key={index}
-              abteilung={devForm.department}
-              job={devForm.education}
-              date={this.doFormatDate(devForm.createdAt)}
-              version={devForm.version}
+              isHeader={true}
+              abteilung="Abteilung"
+              job="Ausbildungsberuf"
+              date="Erstellungsdatum"
+              version="Version"
             />
-          );
-        })}
+            {developmentForms.map((devForm, index) => {
+              return (
+                <ListItem
+                  key={index}
+                  abteilung={devForm.department}
+                  job={devForm.education}
+                  date={this.doFormatDate(devForm.createdAt)}
+                  version={devForm.version}
+                />
+              );
+            })}
+          </div>
+        )}
+        ;
       </div>
     );
+  };
+
+  render() {
+    return <div className={"root"}>{this.getContent()}</div>;
   }
 }
