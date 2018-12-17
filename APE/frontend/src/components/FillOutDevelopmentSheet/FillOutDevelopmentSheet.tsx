4cdd6bdaca6fa2a6bc63aac6f4aa443a88320f7d
Steven Jonscher <steven.jonscher@gmail.com>
2018-12-17 20:41:58 +0100

    Add first styling

--- APE/frontend/src/components/FillOutDevelopmentSheet/FillOutDevelopmentSheet.tsx
+++ APE/frontend/src/components/FillOutDevelopmentSheet/FillOutDevelopmentSheet.tsx
@@ -1,4 +1,12 @@
 import { FillDevelopmentSheetComponent } from "./FillOutDevelopmentSheetComponent";
 
+export interface State {
+  radioValue: string;
+}
+
+export interface Props {}
+
+export type AllProps = Props;
+
 const connectedFillOutDevelopmentSheet = FillDevelopmentSheetComponent;
 export { connectedFillOutDevelopmentSheet as FillOutDevelopmentSheet };
--- APE/frontend/src/components/FillOutDevelopmentSheet/FillOutDevelopmentSheetComponent.tsx
+++ APE/frontend/src/components/FillOutDevelopmentSheet/FillOutDevelopmentSheetComponent.tsx
@@ -1,5 +1,39 @@
 import * as React from "react";
+import { FormControl, FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
+import { AllProps, State } from "./FillOutDevelopmentSheet";
+import "./FillOutDevelopmentSheetComponent.css";
 
-export const FillDevelopmentSheetComponent = () => {
-  return <div>Test</div>;
-};
+export class FillDevelopmentSheetComponent extends React.Component<AllProps, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      radioValue: "3"
+    };
+  }
+
+  private handleChange = event => {
+    console.log("event: ", event.target.value);
+    this.setState({
+      radioValue: event.target.value
+    });
+  };
+
+  render() {
+    const { radioValue } = this.state;
+
+    return (
+      <div className={"criteria-container"}>
+        <legend className={"criteria-text"}>Test</legend>
+        <FormControl component={"fieldset"}>
+          <RadioGroup name={"test"} onChange={this.handleChange} value={radioValue} row={true}>
+            <FormControlLabel value={"1"} control={<Radio color={"primary"} />} label={""} />
+            <FormControlLabel value={"2"} control={<Radio color={"primary"} />} label={""} />
+            <FormControlLabel value={"3"} control={<Radio color={"primary"} />} label={""} />
+            <FormControlLabel value={"4"} control={<Radio color={"primary"} />} label={""} />
+            <FormControlLabel value={"5"} control={<Radio color={"primary"} />} label={""} />
+          </RadioGroup>
+        </FormControl>
+      </div>
+    );
+  }
+}
