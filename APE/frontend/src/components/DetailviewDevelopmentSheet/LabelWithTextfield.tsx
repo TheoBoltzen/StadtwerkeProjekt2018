fb0fd43e81f8f434b7d7e7a20530d3bc9486c75e
glen.j.wider <glen.j.wider@student.fh-kiel.de>
2018-12-17 11:18:16 +0100

    colour of LabelWithTextField adjusted

--- APE/frontend/src/components/DetailviewDevelopmentSheet/LabelWithTextfield.tsx
+++ APE/frontend/src/components/DetailviewDevelopmentSheet/LabelWithTextfield.tsx
@@ -1,29 +1,44 @@
 import * as React from "react";
 import "./LabelWithTextfield.css";
 import { TextField } from "@material-ui/core";
+import { createStyles } from "@material-ui/core/es";
+import withStyles from "@material-ui/core/es/styles/withStyles";
+import createMuiTheme from "@material-ui/core/es/styles/createMuiTheme";
+import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
 
-/*const styles = theme =>
-    createStyles({
-        container: {
-            display: 'flex',
-            flexWrap: 'wrap',
-        },
-        textField: {
-            marginLeft: theme.spacing.unit,
-            marginRight: theme.spacing.unit,
-        },
-        dense: {
-            marginTop: 16,
-        },
-        menu: {
-            width: 200,
-        },
-    });
+const styles = theme =>
+  createStyles({
+    margin: {
+      margin: theme.spacing.unit,
+      backgroundColor: "white"
+    },
+    cssLabel: {
+      "&$cssFocused": {
+        color: "#00a8e1"
+      }
+    },
+    cssFocused: {},
+    cssUnderline: {
+      "&:after": {
+        borderBottomColor: "#00a8e1"
+      }
+    },
+    cssOutlinedInput: {
+      "&$cssFocused $notchedOutline": {
+        borderColor: "#00a8e1"
+      }
+    },
+    notchedOutline: {}
+  });
 
-interface Props extends WithStyles<typeof styles>{
-  name?: string;
-  content?: string;
-}*/
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00a8e1"
+    }
+  },
+  typography: { useNextVariants: true }
+});
 
 interface Props {
   name?: string;
@@ -34,15 +49,29 @@ export const LabelWithTextfield = (props: Props) => {
   const { name, content } = props;
 
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       <TextField
-        className="textfield-color"
+        /*InputLabelProps={{
+              classes: {
+                  root: classes.cssLabel
+
+              },
+          }}
+          InputProps={{
+              classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+              },
+          }}*/
         color="white"
         margin="normal"
         label={name}
         variant={"outlined"}
         value={content}
       />
-    </div>
+    </MuiThemeProvider>
   );
 };
+
+export default withStyles(styles)(LabelWithTextfield);
