da56b469398bc779fd30459f4820cd2ea312dc82
Tim Schierwater <schierwatertim@gmail.com>
2018-12-19 11:58:14 +0100

    redux things implemented succesfully

--- APE/frontend/src/components/Trainees/TraineeView.tsx
+++ APE/frontend/src/components/Trainees/TraineeView.tsx
@@ -3,6 +3,7 @@ import { DevelopmentForm } from "../../types";
 import { ApplicationState } from "../../redux/reducers";
 import { getAll } from "../../redux/actions/development-forms-actions";
 import { connect } from "react-redux";
+import { setTraineeDevelopmentSheet } from "../../redux/actions";
 
 export interface State {
   visibilityIndex: boolean;
@@ -15,25 +16,31 @@ interface Props {}
 interface ReduxStateProps {
   readonly loading: boolean;
   readonly developmentForms: DevelopmentForm[];
+  readonly user: any;
 }
 
 interface ReduxDispatchProps {
   readonly getAllDevForms: () => void;
+  readonly setAssignment: (username: string, devSheetID: string) => void;
 }
 
 export type AllProps = Props & ReduxStateProps & ReduxDispatchProps;
 
 const mapStateToProps = (state: ApplicationState): ReduxStateProps => {
   const { loading, developmentForms } = state.developmentFormsReducer;
+  const { user } = state.authenticationReducer;
   return {
     loading,
+    user: (user as any).token ? user : JSON.parse(user as any),
     developmentForms
   };
 };
 
 const mapDispatchToProps = (dispatch): ReduxDispatchProps => {
   return {
-    getAllDevForms: () => dispatch(getAll())
+    getAllDevForms: () => dispatch(getAll()),
+    setAssignment: (username, devSheetID) =>
+      dispatch(setTraineeDevelopmentSheet(username, devSheetID))
   };
 };
 
