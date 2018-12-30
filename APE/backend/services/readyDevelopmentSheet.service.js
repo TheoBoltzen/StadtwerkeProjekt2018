b409ae8de4895f113481b91e83ea221ab109190e
Theo Boltzen <theo-lukas@boltzen.de>
2018-12-30 16:58:48 +0100

    full user devsheet request added

--- APE/backend/services/readyDevelopmentSheet.service.js
+++ APE/backend/services/readyDevelopmentSheet.service.js
@@ -11,7 +11,8 @@ const competence = require("./competence.service");
 module.exports = {
   update,
   create,
-  getById
+  getemptyById,
+  getfullById
 };
 
 async function create(devSheetParam) {
@@ -175,7 +176,7 @@ async function update(devSheetParam) {
       console.log(err);
     });
 }
-async function getById(devsheetparam) {
+async function getemptyById(devsheetparam) {
   let identifier;
 
   await ReadyDevSheet.findOne({
@@ -322,6 +323,187 @@ async function getById(devsheetparam) {
               name: result[j].Competence.name,
               goalCross: result[j].goalcross,
               ynAnswer: result[j].Competence.ynAnswer
+            };
+            competencycategories[i].children[m].children[s].children.push(
+              competences
+            );
+          }
+        }
+      }
+    }
+  }
+  info.content.push(competencycategories);
+  return info;
+}
+async function getfullById(devsheetparam) {
+  let identifier;
+
+  await ReadyDevSheet.findOne({
+    where: {
+      DevelopmentSheetId: devsheetparam.id
+    },
+    attributes: [[Sequelize.fn("max", Sequelize.col("version")), "version"]]
+  }).then(function(result) {
+    identifier = result.version;
+  });
+
+  let userid = await db.userDevelopmentSheet.findOne({
+    where: {
+      DevelopmentSheetId: devsheetparam.id,
+      TraineeUsername: devsheetparam.trainee,
+      TrainerUsername: devsheetparam.trainer
+    },
+    attributes: ["id"],
+    required: true
+  });
+  let userasso = await db.userDevelopmentSheetAssociation.findAll({
+    where: { UserDevelopmentSheetId: userid.id },
+    attributes: [
+      "ReadyDevelopmentSheetId",
+      "assessmentTRAINER",
+      "assessmentTRAINEE"
+    ],
+    required: true
+  });
+  let devresult = await db.readyDevelopmentSheet.findAll({
+    where: { version: identifier, DevelopmentSheetId: devsheetparam.id },
+    include: [
+      {
+        model: db.developmentSheet,
+        attributes: ["department", "education"],
+        required: true
+      }
+    ]
+  });
+  let result = await db.readyDevelopmentSheet.findAll({
+    attributes: ["id", "goalcross", "version", "DevelopmentSheetId"],
+    required: true,
+    /*order:  Sequelize.col("id"),*/
+    where: { version: identifier, DevelopmentSheetId: devsheetparam.id },
+    include: [
+      {
+        model: db.competence,
+        attributes: ["name", "ynAnswer"],
+        required: true,
+        include: [
+          {
+            model: db.subCategory,
+            attributes: ["name"],
+            required: true,
+            include: [
+              {
+                model: db.mainCategory,
+                attributes: ["name"],
+                required: true,
+                include: [
+                  {
+                    model: db.competencyCategory,
+                    attributes: ["name"],
+                    required: true
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  });
+  //collecting ReadyDevSheetInfos
+  let content = new Array();
+  let info = {
+    devSheetid: result[0].DevelopmentSheetId,
+    version: result[0].version,
+    department: devresult[0].DevelopmentSheet.department,
+    education: devresult[0].DevelopmentSheet.education,
+    trainer: devsheetparam.trainer,
+    trainee: devsheetparam.trainee,
+    content: []
+  };
+
+  let competencycategories = [];
+
+  let i = 0;
+  let j = 0;
+  let s = 0;
+  let m = 0;
+
+  for (i = 0; i < result.length; i++) {
+    if (
+      !competencycategories.find(c => {
+        return (
+          c.name ===
+          result[i].Competence.SubCategory.MainCategory.CompetencyCategory.name
+        );
+      })
+    ) {
+      let comp = {
+        name:
+          result[i].Competence.SubCategory.MainCategory.CompetencyCategory.name,
+        children: []
+      };
+      competencycategories.push(comp);
+    }
+  }
+  for (i = 0; i < competencycategories.length; i++) {
+    for (j = 0; j < result.length; j++) {
+      if (
+        !competencycategories[i].children.find(c => {
+          return c.name === result[j].Competence.SubCategory.MainCategory.name;
+        }) &&
+        result[j].Competence.SubCategory.MainCategory.CompetencyCategory
+          .name === competencycategories[i].name
+      ) {
+        let comp = {
+          name: result[j].Competence.SubCategory.MainCategory.name,
+          children: []
+        };
+        competencycategories[i].children.push(comp);
+      }
+    }
+  }
+  for (i = 0; i < competencycategories.length; i++) {
+    for (m = 0; m < competencycategories[i].children.length; m++) {
+      for (j = 0; j < result.length; j++) {
+        if (
+          !competencycategories[i].children[m].children.find(c => {
+            return c.name === result[j].Competence.SubCategory.name;
+          }) &&
+          result[j].Competence.SubCategory.MainCategory.name ===
+            competencycategories[i].children[m].name
+        ) {
+          let sub = {
+            name: result[j].Competence.SubCategory.name,
+            children: []
+          };
+          competencycategories[i].children[m].children.push(sub);
+        }
+      }
+    }
+  }
+  for (i = 0; i < competencycategories.length; i++) {
+    for (m = 0; m < competencycategories[i].children.length; m++) {
+      for (
+        s = 0;
+        s < competencycategories[i].children[m].children.length;
+        s++
+      ) {
+        for (j = 0; j < result.length; j++) {
+          if (
+            !competencycategories[i].children[m].children[s].children.find(
+              c => {
+                return c.name === result[j].Competence.name;
+              }
+            ) &&
+            result[j].Competence.SubCategory.name ===
+              competencycategories[i].children[m].children[s].name
+          ) {
+            let competences = {
+              name: result[j].Competence.name,
+              goalCross: result[j].goalcross,
+              ynAnswer: result[j].Competence.ynAnswer,
+              trainerassessment: userasso[j].assessmentTRAINER,
+              traineeassessment: userasso[j].assessmentTRAINEE
             };
             competencycategories[i].children[m].children[s].children.push(
               competences
