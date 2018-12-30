87e31a96df60e43b02472679a309c91468319b5f
Steven Jonscher <steven.jonscher@gmail.com>
2018-12-30 23:56:59 +0100

    Add component elements and style

--- APE/frontend/src/components/Trainees/TraineesComponent.tsx
+++ APE/frontend/src/components/Trainees/TraineesComponent.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { AllProps } from "./Trainees";
+import { CircularProgress } from "@material-ui/core";
+import { ListItemTrainee } from "./ListItemTrainee";
+import "./TraineesComponent.css";
 
 export class TraineesComponent extends React.Component<AllProps> {
   constructor(props: AllProps) {
@@ -11,7 +14,28 @@ export class TraineesComponent extends React.Component<AllProps> {
   }
 
   render() {
-    console.log("Props: ", this.props);
-    return <div>Auszubildende</div>;
+    const { loading, trainees } = this.props;
+    return loading ? (
+      <CircularProgress />
+    ) : (
+      <div className={"frameCenterTraineeTab"}>
+        <ListItemTrainee
+          isHeader={true}
+          name={"Name"}
+          username={"Kennung"}
+          firstname={"Vorname"}
+          education={"Ausbildungsberuf"}
+        />
+        {trainees.map((trainee, index) => (
+          <ListItemTrainee
+            key={index}
+            name={trainee.lastname}
+            username={trainee.username}
+            firstname={trainee.firstname}
+            education={trainee.education}
+          />
+        ))}
+      </div>
+    );
   }
 }
