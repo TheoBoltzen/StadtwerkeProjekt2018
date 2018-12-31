e9785f0e44c72f7525b389565f9cfbec453beadb
Steven Jonscher <steven.jonscher@gmail.com>
2018-12-31 18:07:21 +0100

    Add button to add user

--- APE/frontend/src/components/UserAdministration/UserAdministrationComponent.tsx
+++ APE/frontend/src/components/UserAdministration/UserAdministrationComponent.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "./UserAdministrationComponent.css";
-import { CircularProgress } from "@material-ui/core";
+import { Button, CircularProgress } from "@material-ui/core";
 import { ListItem } from "./ListItem";
 import { AllProps } from "./UserAdministration";
 
@@ -13,31 +13,48 @@ export class UserAdministrationComponent extends React.Component<AllProps, {}> {
     this.props.getAllUsers();
   }
 
+  private openAddUserModal = () => {
+    console.log("test");
+  };
+
   render() {
     const { users, loading } = this.props;
 
     return loading ? (
       <CircularProgress />
     ) : (
-      <div className={"frame center"}>
-        <ListItem
-          isHeader={true}
-          username={"Kennung"}
-          name={"Name"}
-          firstname={"Vorname"}
-          job={"Tätigkeit"}
-        />
-        {users.map((user, index) => {
-          return (
-            <ListItem
-              key={index}
-              username={user.username}
-              name={user.lastname}
-              firstname={user.firstname}
-              job={user.job}
-            />
-          );
-        })}
+      <div className={"root-user-administration"}>
+        <div className={"add-user-button-container"}>
+          <div />
+          <Button
+            variant={"contained"}
+            color={"primary"}
+            className={"add-user-button"}
+            onClick={this.openAddUserModal}>
+            Benutzer anlegen
+          </Button>
+        </div>
+
+        <div className={"frame center"}>
+          <ListItem
+            isHeader={true}
+            username={"Kennung"}
+            name={"Name"}
+            firstname={"Vorname"}
+            job={"Tätigkeit"}
+          />
+          {users.map((user, index) => {
+            return (
+              <ListItem
+                key={index}
+                username={user.username}
+                name={user.lastname}
+                firstname={user.firstname}
+                job={user.job}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
