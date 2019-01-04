a428781667140c5509668fd2487df715911a39c8
Steven Jonscher <steven.jonscher@gmail.com>
2019-01-04 15:48:21 +0100

    Add action + reducer

--- APE/frontend/src/redux/actions/trainees-tab-actions.ts
+++ APE/frontend/src/redux/actions/trainees-tab-actions.ts
@@ -1,8 +1,9 @@
-import { traineeTabConstants } from "../../constants";
-import { Trainee } from "../../types";
+import { traineeTabConstants, trainerDevelopmentFormConstants } from "../../constants";
+import { ConnectedDevSheetFetch, Trainee } from "../../types";
 import { Dispatch } from "redux";
 import { getAllTraineesService } from "../../services";
 import { errorAlert } from "./alert";
+import { getAllConnectedDevelopmentSheetsService } from "../../services/trainer-connected-developmentsheets-services";
 
 export const getAllTrainees = () => {
   const request = () => {
@@ -22,6 +23,35 @@ export const getAllTrainees = () => {
 
     getAllTraineesService().then(
       trainees => dispatch(success(trainees)),
+      error => {
+        dispatch(failure(error.toString()));
+        dispatch(errorAlert(error.toString()));
+      }
+    );
+  };
+};
+
+export const getAllConnectedDevSheets = () => {
+  const request = () => {
+    return { type: trainerDevelopmentFormConstants.GETALL_CONNECTEDDEVSHEETS_REQUEST };
+  };
+
+  const success = (connectedDevSheets: ConnectedDevSheetFetch[]) => {
+    return {
+      type: trainerDevelopmentFormConstants.GETALL_CONNECTEDDEVSHEETS_SUCCESS,
+      connectedDevSheets
+    };
+  };
+
+  const failure = (error: string) => {
+    return { type: trainerDevelopmentFormConstants.GETALL_CONNECTEDDEVSHEETS_FAILURE, error };
+  };
+
+  return (dispatch: Dispatch) => {
+    dispatch(request());
+
+    getAllConnectedDevelopmentSheetsService().then(
+      connectedDevSheets => dispatch(success(connectedDevSheets)),
       error => {
         dispatch(failure(error.toString()));
         dispatch(errorAlert(error.toString()));
